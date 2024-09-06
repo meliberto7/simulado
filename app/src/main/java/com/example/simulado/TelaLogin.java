5a74c5fdf7fdd863e4f8f80391b8831dfedb9893
meliberto7 <lendrioto@gmail.com>
2024-09-05 19:15:13 -0700

    Ids modificado dos objetos

--- app/src/main/java/com/example/simulado/TelaLogin.java
+++ app/src/main/java/com/example/simulado/TelaLogin.java
@@ -1,6 +1,8 @@
 package com.example.simulado;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,6 +11,11 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class TelaLogin extends AppCompatActivity {
+
+    private EditText editEmail, editSenha;
+    private Button buttonEntrar;
+    private int cont = 0;
+    private boolean very = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,5 +27,8 @@ public class TelaLogin extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        editEmail = findViewById()
+
     }
 }
