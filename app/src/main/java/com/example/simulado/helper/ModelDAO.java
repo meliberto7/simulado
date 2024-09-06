718d15ceb804c95de60fed9b5da9d3796b2ad1e8
meliberto7 <lendrioto@gmail.com>
2024-09-05 19:26:57 -0700

    Verificação Logar com banco criado - DAO

--- app/src/main/java/com/example/simulado/helper/ModelDAO.java
+++ app/src/main/java/com/example/simulado/helper/ModelDAO.java
@@ -1,4 +1,46 @@
 package com.example.simulado.helper;
 
+import android.content.Context;
+import android.database.Cursor;
+import android.database.SQLException;
+import android.database.sqlite.SQLiteDatabase;
+
 public class ModelDAO {
+
+    private SQLiteDatabase altera, le;
+
+    public ModelDAO(Context context) {
+
+        DBHelper helper = new DBHelper(context);
+
+        altera = helper.getWritableDatabase();
+        le = helper.getReadableDatabase();
+
+    }
+
+    public boolean logar(String email, String senha) {
+
+        boolean very = false;
+
+        try{
+
+            String sql = "SELECT * FROM usuarios WHERE email = ? AND senha = ?";
+
+            Cursor cursor = altera.rawQuery(sql, new String[]{email, senha});
+
+            if (cursor.moveToNext()) {
+
+                very = true;
+
+            }
+
+            cursor.close();
+
+        }catch (SQLException e){
+            e.printStackTrace();
+        }
+
+        return very;
+    }
+
 }
